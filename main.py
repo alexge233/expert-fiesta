2b4ed581013d9321b6d03ae0f4093ccf140fa682
Alex Ge <alexge233@hotmail.com>
2024-08-21 13:41:31 +0100

    typo fix

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import torch
 assert torch.cuda.is_available() is True
 assert torch.cuda.device_count() > 1
 print(torch.cuda.get_device_name(0))
-print(t.backends.cudnn.enabled)
+print(torch.backends.cudnn.enabled)
